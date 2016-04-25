daef720e3d79ee7117a64b60272c4e04208e52fa
Joseph Smith <joseph.smith@guardian.co.uk>
2016-04-25 18:26:08 +0100

    Feedback buttons

--- src/js/embed.js
+++ src/js/embed.js
@@ -1,6 +1,7 @@
 import iframeMessenger from 'guardian/iframe-messenger'
 import reqwest from 'reqwest'
 import embedHTML from './text/embed.html!text'
+import thankYouHTML from './text/thankYou.html!text'
 
 window.init = function init(el, config) {
     iframeMessenger.enableAutoResize();
@@ -13,4 +14,9 @@ window.init = function init(el, config) {
     //     crossOrigin: true,
     //     success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
     // });
+
+    el.querySelector('.js-like').addEventListener('click', ev => {
+        let feedback = ev.currentTarget.parentNode;
+        feedback.innerHTML = thankYouHTML;
+    });
 };
