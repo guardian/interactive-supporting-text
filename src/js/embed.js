d2374a4ba3a06c036ebbecff24c6e9e24e2918c3
Joseph Smith <joseph.smith@guardian.co.uk>
2016-04-28 12:31:08 +0100

    Styling tweaks and JS refactor

--- src/js/embed.js
+++ src/js/embed.js
@@ -5,6 +5,10 @@ import thankYouHTML from './text/thankYou.html!text'
 import reasonHTML from './text/reason.html!text'
 
 window.init = function init(el, config) {
+    function q(selectorString) {
+        return [].slice.apply(el.querySelectorAll(selectorString));
+    }
+
     iframeMessenger.enableAutoResize();
 
     el.innerHTML = embedHTML;
@@ -17,27 +21,19 @@ window.init = function init(el, config) {
         success: (resp) => {
             let reasonsToLeave = resp && resp.sheets && resp.sheets.reasonsToLeave;
             console.log(reasonsToLeave);
+            
             if (reasonsToLeave && reasonsToLeave.length) {
                 let htmlArray = reasonsToLeave.map(reason => {
                     return reasonHTML.replace(/%youSay%/g, reason.youSay).replace(/%theySay%/g, reason.theySay);
                 });
                 el.querySelector('.reasons').innerHTML = htmlArray.join("\n");
 
-                let title = reasonsToLeave[0].title;
-                if (title) {
-                    let e = el.querySelector('.brexit__title');
-                    e.innerHTML = e.innerHTML.replace(/%title%/g, title);
-                }
-                let articleLinkText = reasonsToLeave[0].articleLinkText;
-                if (articleLinkText) {
-                    let e = el.querySelector('.brexit__button');
-                    e.innerHTML = e.innerHTML.replace(/%articleLinkText%/g, articleLinkText);
-                }
-                let feedbackQuestion = reasonsToLeave[0].feedbackQuestion;
-                if (feedbackQuestion) {
-                    let e = el.querySelector('.brexit__feedback');
-                    e.innerHTML = e.innerHTML.replace(/%feedbackQuestion%/g, feedbackQuestion);
-                }
+                ['title', 'articleLinkText', 'feedbackQuestion'].forEach(field => {
+                    let text = reasonsToLeave[0][field];
+                    if (text) {
+                        q('[data-source-'+field+']').forEach(el => el.innerHTML = text);
+                    }
+                })
             } else {
                 console.log('bad JSON response');
                 console.log(resp);
@@ -45,17 +41,13 @@ window.init = function init(el, config) {
         }
     });
 
-    function arr(nodeList) {
-        return [].slice.apply(nodeList);
-    }
-
-    arr(el.querySelectorAll('.js-feedback')).forEach(el => el.addEventListener('click', ev => {
+    q('.js-feedback').forEach(el => el.addEventListener('click', ev => {
         let el = ev.currentTarget;
         let feedback = el.parentNode;
         feedback.innerHTML = thankYouHTML.replace(/%surveyHref%/, el.getAttribute('data-survey-href'));
     }));
 
-    arr(el.querySelectorAll('[data-track]')).forEach(el => el.addEventListener('click', ev => {
+    q('[data-track]').forEach(el => el.addEventListener('click', ev => {
         let trackingCode = ev.currentTarget.getAttribute('data-track');
         console.log('tracking event ' + trackingCode);
         _satellite.track(trackingCode);
