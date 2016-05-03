ff74e085b9d30e2e70c8eadfa77e29e6b3e7275e
Joseph Smith <joseph.smith@guardian.co.uk>
2016-05-03 17:03:16 +0100

    Clear out the dead wood

--- src/js/embed.js
+++ src/js/embed.js
@@ -7,23 +7,6 @@ import questionAndAnswer from './text/questionAndAnswer.dot.html!text'
 
 import dot from 'olado/doT'
 
-var variants = {
-    // reasonsToLeave: {
-    //    // TODO: change to prod path in production
-    //     dataSource: 'https://interactive.guim.co.uk/docsdata-test/1zsqQf4mq8fsAkZAXnoSCNpap2hykFDA3Cm3HaI9qe8k.json',
-    //     template: reasonsTemplate
-    // },
-    // reasonsToStay: {
-    //     dataSource: 'https://interactive.guim.co.uk/docsdata-test/1zsqQf4mq8fsAkZAXnoSCNpap2hykFDA3Cm3HaI9qe8k.json',
-    //     template: reasonsTemplate
-    // },
-    questionAndAnswer: {
-        dataSource: 'https://interactive.guim.co.uk/docsdata-test/1zsqQf4mq8fsAkZAXnoSCNpap2hykFDA3Cm3HaI9qe8k.json',
-        template: questionAndAnswer
-    }
-};
-
-
 function q(selectorString) {
     return [].slice.apply(document.querySelectorAll(selectorString));
 }
@@ -45,16 +28,12 @@ function bindEventHandlers() {
 window.init = function init(el, config) {
     iframeMessenger.enableAutoResize();
 
-    // el.innerHTML = embedHTML;
-
-    // var variantName = window.location.search.replace('?', '');
     var query = window.location.search.replace('?', '').split('&');
     var params = {};
     query.forEach(q => {
         let keyVal = q.split('=');
         params[keyVal[0]] = keyVal[1];
     });
-    // var variant = (params['variant'] && variants[params['variant']]) || variants['questionAndAnswer'];
 
     var sheet = params['sheet'];
     var row = parseInt(params['row']) - 2;
@@ -65,12 +44,8 @@ window.init = function init(el, config) {
         success: (resp) => {
             var data = resp && resp.sheets && resp.sheets[sheet];
 
-            console.log('data');
-            console.log(data);
             if (data && data[row]) {
                 let fn = dot.template(questionAndAnswer);
-                console.log('data row');
-                console.log(data[row]);
                 el.innerHTML = fn({
                     data: data[row]
                 });
