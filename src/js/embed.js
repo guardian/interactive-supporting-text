4ed437b1c4c5e4a7434c455fb7741954e0b6b185
Joseph Smith <joseph.smith@guardian.co.uk>
2016-05-03 16:47:45 +0100

    Fetch question and answer pairs from Google Doc

--- src/js/embed.js
+++ src/js/embed.js
@@ -4,19 +4,24 @@ import embedHTML from './text/embed.html!text'
 import thankYouHTML from './text/thankYou.html!text'
 import reasonHTML from './text/reason.html!text'
 
-import reasonsTemplate from './text/reasons.dot.html!text'
+// import reasonsTemplate from './text/reasons.dot.html!text'
+import questionAndAnswer from './text/questionAndAnswer.dot.html!text'
 
 import dot from 'olado/doT'
 
 var variants = {
-    reasonsToLeave: {
-       // TODO: change to prod path in production
+    // reasonsToLeave: {
+    //    // TODO: change to prod path in production
+    //     dataSource: 'https://interactive.guim.co.uk/docsdata-test/1zsqQf4mq8fsAkZAXnoSCNpap2hykFDA3Cm3HaI9qe8k.json',
+    //     template: reasonsTemplate
+    // },
+    // reasonsToStay: {
+    //     dataSource: 'https://interactive.guim.co.uk/docsdata-test/1zsqQf4mq8fsAkZAXnoSCNpap2hykFDA3Cm3HaI9qe8k.json',
+    //     template: reasonsTemplate
+    // },
+    questionAndAnswer: {
         dataSource: 'https://interactive.guim.co.uk/docsdata-test/1zsqQf4mq8fsAkZAXnoSCNpap2hykFDA3Cm3HaI9qe8k.json',
-        template: reasonsTemplate
-    },
-    reasonsToStay: {
-        dataSource: 'https://interactive.guim.co.uk/docsdata-test/1zsqQf4mq8fsAkZAXnoSCNpap2hykFDA3Cm3HaI9qe8k.json',
-        template: reasonsTemplate
+        template: questionAndAnswer
     }
 };
 
@@ -44,33 +49,40 @@ window.init = function init(el, config) {
 
     // el.innerHTML = embedHTML;
 
-    var variantName = window.location.hash.replace('#', '') || 'reasonsToLeave';
-    var variant = variants[variantName];
-    if (variant) {
-        reqwest({
-            url: variant.dataSource,
-            type: 'json',
-            crossOrigin: false,
-            success: (resp) => {
-                let data = resp && resp.sheets && resp.sheets[variantName];
-                let metaData = resp && resp.sheets && resp.sheets[variantName+'Meta'];
+    // var variantName = window.location.search.replace('?', '');
+    var query = window.location.search.replace('?', '').split('&');
+    var params = {};
+    query.forEach(q => {
+        let keyVal = q.split('=');
+        params[keyVal[0]] = keyVal[1];
+    });
+    // var variant = (params['variant'] && variants[params['variant']]) || variants['questionAndAnswer'];
 
-                if (data && data.length && metaData && metaData.length) {
-                    let fn = dot.template(variant.template);
-                    el.innerHTML = fn({
-                        data: data,
-                        metaData: metaData[0]
-                    });
+    var sheet = params['sheet'];
+    var row = parseInt(params['row']) - 2;
+    reqwest({
+        url: 'https://interactive.guim.co.uk/docsdata-test/1zsqQf4mq8fsAkZAXnoSCNpap2hykFDA3Cm3HaI9qe8k.json',
+        type: 'json',
+        crossOrigin: false,
+        success: (resp) => {
+            var data = resp && resp.sheets && resp.sheets[sheet];
 
-                    bindEventHandlers();
-                } else {
-                    console.log('bad JSON response');
-                    console.log(resp);
-                }
+            console.log('data');
+            console.log(data);
+            if (data && data[row]) {
+                let fn = dot.template(questionAndAnswer);
+                console.log('data row');
+                console.log(data[row]);
+                el.innerHTML = fn({
+                    data: data[row]
+                });
+
+                bindEventHandlers();
+            } else {
+                console.log('bad JSON response');
+                console.log(resp);
             }
-        });
-    } else {
-        console.log('Invalid variant '+variantName);
-    }
+        }
+    });
 };
 
