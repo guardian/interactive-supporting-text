6c9e436db81c9a8b8591d0d526ce1d3561d789fe
Simon Adcock <simon.adcock@guardian.co.uk>
2016-05-31 17:53:08 +0100

    Extract carousel size to application.json, inc to 6

--- src/js/formats/bigNumberCarousel.js
+++ src/js/formats/bigNumberCarousel.js
@@ -1,5 +1,6 @@
 import carouselTemplate from '../text/carousel.dot.html!text';
 import buildCarousel from './helpers/buildCarousel';
+import { maxCarouselSize } from '../../config/application.json!json';
 
 export default {
     preprocess(data) {
@@ -7,12 +8,11 @@ export default {
         const { survey_like, survey_dislike } = data;
         const newData = { survey_like, survey_dislike };
         const bigNumberMatcher = /(\d+|\/|\.)/g;
-        const max = 5;
         let i = 1;
         let bigNumber;
         let content;
 
-        for (; i <= max; i += 1) {
+        for (; i <= maxCarouselSize; i += 1) {
             bigNumber = data[`headline${i}`];
             content = data[`content${i}`];
             if (content && bigNumber) {
--- src/js/formats/textCarousel.js
+++ src/js/formats/textCarousel.js
@@ -1,17 +1,17 @@
 import carouselTemplate from '../text/carousel.dot.html!text';
 import buildCarousel from './helpers/buildCarousel';
+import { maxCarouselSize } from '../../config/application.json!json';
 
 export default {
     preprocess(data) {
         const slides = [];
         const { survey_like, survey_dislike } = data;
         const newData = { survey_like, survey_dislike };
-        const max = 5;
         let i = 1;
         let header;
         let content;
 
-        for (; i <= max; i += 1) {
+        for (; i <= maxCarouselSize; i += 1) {
             header = data[`headline${i}`];
             content = data[`content${i}`];
             if (typeof header !== 'undefined' && typeof content !== 'undefined') {
