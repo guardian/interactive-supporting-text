9151c79ded8b0fb530e756cd0778275fbfba79cd
Simon Adcock <simon.adcock@guardian.co.uk>
2016-06-02 13:12:09 +0100

    Basic structure for catch-me-up atom

---
+++ src/js/formats/catchMeUp.js
@@ -0,0 +1,9 @@
+import catchMeUpTemplate from '../text/catchMeUp.dot.html!text';
+
+export default {
+    preprocess({ headline1: header, content1: content, survey_like, survey_dislike }) {
+        return { header, content, survey_like, survey_dislike };
+    },
+    postRender() {},
+    template: catchMeUpTemplate,
+};
