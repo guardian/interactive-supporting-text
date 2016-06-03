14155f629b2de3b706e1bff180bebd156fe444f6
Joseph Smith <joseph.smith@guardian.co.uk>
2016-06-03 17:24:34 +0100

    Get catch me up URL from spreadsheet

--- src/js/formats/catchMeUp.js
+++ src/js/formats/catchMeUp.js
@@ -1,13 +1,13 @@
 import catchMeUpTemplate from '../text/catchMeUp.dot.html!text';
 
 export default {
-    preprocess({ headline1: header, content1: content, survey_like, survey_dislike }) {
+    preprocess({ headline1: header, content1: content, headline2: url, survey_like, survey_dislike }) {
         return {
             header,
             content,
             survey_like,
             survey_dislike,
-            url: 'http://theguardian.com/uk',
+            url: url || 'http://theguardian.com/uk',
         };
     },
     postRender() {
