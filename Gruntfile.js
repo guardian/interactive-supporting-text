1848cf9deb400d09b99713b1d3fed461e8a9d49a
Roberto Tyley <roberto.tyley@theguardian.com>
2016-06-03 11:08:50 +0100

    Change the deployable files so they don't contain their deployed path
    For RiffRaff we want to have a deployable artifact that will work for
    *any* environment, so it doesn't hardcode the destination into it's own
    deployable files, like this:
    
    ```
    embed.html
    6:    <link rel="stylesheet" type="text/css" href="https://interactive.guim.co.uk/testing/2016/05/brexit-companion/1464873660958/embed.css" />
    10:    <script src="https://interactive.guim.co.uk/testing/2016/05/brexit-companion/1464873660958/embed.js"></script>
    12:        init(document.querySelector('.js-interactive'), {'assetPath': 'https://interactive.guim.co.uk/testing/2016/05/brexit-companion/1464873660958'});
    ```

--- Gruntfile.js
+++ Gruntfile.js
@@ -216,7 +216,7 @@ module.exports = function(grunt) {
             s3: grunt.file.readJSON('./cfg/s3.json'),
             timestamp: Date.now(),
             jspmFlags: '-m',
-            assetPath: '<%= visuals.s3.domain %><%= visuals.s3.path %>/<%= visuals.timestamp %>'
+            assetPath: '<%= visuals.timestamp %>'
         });
     })
 
