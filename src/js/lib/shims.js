f79ccd03a41e90a023cd792ec122d0fdca0c22ca
Joseph Smith <joseph.smith@guardian.co.uk>
2016-06-22 17:47:53 +0100

    Make feedback section look OK at small widths

--- src/js/lib/shims.js
+++ src/js/lib/shims.js
@@ -1,7 +1,39 @@
-if (!('remove' in Element.prototype)) {
+/* eslint-disable */
+
+if (typeof Element.prototype.remove !== 'function') {
     Element.prototype.remove = function remove() {
         if (this.parentNode) {
             this.parentNode.removeChild(this);
         }
+    };
+}
+
+// element-closest | CC0-1.0 | github.com/jonathantneal/closest
+if (typeof Element.prototype.matches !== 'function') {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector || function matches(selector) {
+        var element = this;
+        var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+        var index = 0;
+
+        while (elements[index] && elements[index] !== element) {
+            ++index;
+        }
+
+        return Boolean(elements[index]);
+    };
+}
+if (typeof Element.prototype.closest !== 'function') {
+    Element.prototype.closest = function closest(selector) {
+        var element = this;
+
+        while (element && element.nodeType === 1) {
+            if (element.matches(selector)) {
+                return element;
+            }
+
+            element = element.parentNode;
+        }
+
+        return null;
     };
 }
