bb6eda843f0e4d9818aabe8ada2be9221f721e65
Simon Adcock <simon.adcock@guardian.co.uk>
2016-06-23 12:03:59 +0100

    Fonts

--- src/js/embed.js
+++ src/js/embed.js
@@ -7,7 +7,10 @@ window.init = function init(parentEl) {
     const templateFn = dot.template(template);
     const templateData = {
         header: 'Join us as we launch an innovative form of collaborative journalism',
-        content: 'Follow a Guardian journalist as they dig into a topic off the beaten news track, track their working out as they go, and contribute ideas, suggestions and feedback. With weekly updates, we hope to keep interested readers updated but not inundated on the issues they care about, and give them the chance to collaborate in our reporting.',
+        content: `Follow a Guardian journalist as they dig into a topic off the beaten news track, 
+            track their working out as they go, and contribute ideas, suggestions and feedback. 
+            With weekly updates, we hope to keep interested readers updated but not inundated on 
+            the issues they care about, and give them the chance to collaborate in our reporting.`,
         footer: 'Sign up to the Labour & Liverpool newsletter',
     };
 
